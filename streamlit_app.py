e8c70b3475001effec24ff242e83d34a2b627a6f
Juliana Méndez Rojas <46967761+julianamendez11@users.noreply.github.com>
2024-09-26 10:07:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,25 @@ else:
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
 
-# Add logo at the bottom of the page.
-st.markdown("<br><br><br>", unsafe_allow_html=True)  # Add some space before the logo
-logo_path = "cuesta-logo.png" 
-st.image(logo_path, caption="Cuesta Data Team", use_column_width=True)
+st.markdown(
+"""
+<style>
+.image-container {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+}
+.image-container img {
+    margin-right: 10px;
+}
+</style>
+""",
+unsafe_allow_html=True
+)
+
+with st.container():
+    st.markdown('<div class="image-container">', unsafe_allow_html=True)
+    st.image("cuesta-logo.png", use_column_width=False, width=250)
+    st.markdown('</div>', unsafe_allow_html=True)
